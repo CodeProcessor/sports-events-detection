51a73bb74e6ea6662173f18336a66e754b8bfd63
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2022-01-23 17:54:22 +0530

    Storage class

---
+++ train_network/Detection/yolov5/storage.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+@Filename:    storage.py
+@Author:      dulanj
+@Time:        23/01/2022 17:46
+"""
+import json
+import logging
+import os
+import sqlite3 as sl
+
+
+class Storage:
+    def __init__(self, db_name):
+        init = not os.path.exists(db_name)
+        self.con = sl.connect(db_name)
+        self.cur = self.con.cursor()
+        if init:
+            self.create_table()
+
+    def create_table(self):
+        self.cur.execute("""
+                CREATE TABLE DATA (
+                    frame_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    data TEXT
+                );
+            """)
+        self.con.commit()
+
+    def insert_data(self, frame_id, data):
+        try:
+            sql = 'INSERT INTO DATA (frame_id, data) values(?, ?)'
+            data = [
+                (frame_id, json.dumps(data))
+            ]
+            self.cur.executemany(sql, data)
+            self.con.commit()
+        except sl.IntegrityError:
+            logging.error('Duplicate frame_id: {}'.format(frame_id))
+
+    def get_data(self, frame_id):
+        sql = 'SELECT data FROM DATA WHERE frame_id = ?'
+        data = self.con.execute(sql, (frame_id,)).fetchone()
+        return json.loads(data[0])
+
+    def __del__(self):
+        self.con.close()
+
+
+if __name__ == '__main__':
+    storage = Storage('storage_1.db')
+    storage.insert_data(1, {'a': 1})
+    storage.insert_data(2, {'b': 2})
+    storage.insert_data(3, {'c': 3})
+    print(storage.get_data(1))
+    print(storage.get_data(2))
+    print(storage.get_data(3))
