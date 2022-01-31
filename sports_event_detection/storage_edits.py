ffdaec2ef0390b06fca2038bd5511b6541cdaef2
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2022-01-31 09:25:15 +0530

    Improvements to storage edits

--- sports_event_detection/storage_edits.py
+++ sports_event_detection/storage_edits.py
@@ -4,19 +4,26 @@
 @Author:      dulanj
 @Time:        27/01/2022 22:20
 """
+from sports_event_detection.common import ModelNames
 from sports_event_detection.storage import Storage
 
 if __name__ == '__main__':
-    storage = Storage('Match#16_CR_&_FC_v_Army_SC_DRL_2019_20_event.db')
-    storage_updated = Storage('Match#16_CR_&_FC_v_Army_SC_DRL_2019_20_event_n.db')
+    storage = Storage('data_storage/Match#16_CR_&_FC_v_Army_SC_DRL_2019_20_play_n.db')
+    storage_updated = Storage('data_storage/Match#16_CR_&_FC_v_Army_SC_DRL_2019_20.db')
     frame_id = 1
     data = storage.get_data(frame_id)
     bulk_data = []
+    delete_frame_ids = []
     while data is not None:
-        data["data"] = data.pop("scrum_lineout_pred")
+        if isinstance(data["data"], dict):
+            data["data"][f"{ModelNames.digital_object_detection_model.name}"] = data.pop("data")
+        else:
+            data["data"] = {f"{ModelNames.digital_object_detection_model.name}": data.pop("data")}
         bulk_data.append(data)
+        delete_frame_ids.append(frame_id)
 
         if len(bulk_data) > 5000:
+            storage_updated.delete_bulk_data(delete_frame_ids)
             storage_updated.insert_bulk_data(bulk_data)
             bulk_data = []
 
