d46cf82a09064bb0548972851875310c78cdfc3f
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2022-04-10 09:16:55 +0530

    Classification model test updated

--- training_pipeline/step3_model_training/clasification/test.py
+++ training_pipeline/step3_model_training/clasification/test.py
@@ -3,6 +3,8 @@
 @Author:      dulanj
 @Time:        2021-08-23 16.12
 """
+import time
+
 import torch
 
 from data_loader import get_test_loader
@@ -10,10 +12,12 @@ from params import configs, DEVICE
 from pretrained_models import initialize_model
 
 
-def test():
-    # model = ClassificationModel(num_classes=configs["data"]["no_of_classes"]).to(DEVICE)
-    model_name = "efficientnet"
-    num_classes = 2
+def main():
+    model_name = configs["model"]["model_name"]
+    num_classes = configs["data"]["no_of_classes"]
+
+    print("Test for the model {} with {} classes".format(model_name, num_classes))
+
     feature_extract = False
     model, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=False)
     model_file_name = configs["model"]["model_load_path"]
@@ -26,40 +30,29 @@ def test():
     model.eval()
     correct = 0
     total = 0
+    pred_time_list = []
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
             images = images.to(DEVICE)
             labels = labels.to(DEVICE)
+            start_time = time.time()
             outputs = model(images)
+            end_time = time.time()
+            pred_time_list.append(end_time - start_time)
             _, predicted = torch.max(outputs.data, 1)
             _, labels = torch.max(labels.data, 1)
-            print("Actual: ", labels.data.cpu().numpy())
-            print("Predicted: ", predicted.cpu().numpy())
+            # print("Actual: ", labels.data.cpu().numpy())
+            # print("Predicted: ", predicted.cpu().numpy())
 
             total += labels.size(0)
             correct += sum(predicted == labels)
-            print("Accuracy: ", 100 * correct / total)
+            # print("Accuracy: ", 100 * correct / total)
 
+    print("Average prediction time: ", round(sum(pred_time_list) * 1000 / len(pred_time_list), 2), "ms")
     print('Accuracy of the network on the {} test images: {}%'.format(total, 100 * correct / total))
-
-    # for x, y in test_loader:
-    #
-    #     x = x.to(DEVICE)
-    #     outputs = model(x)
-    #
-    #     _, predicted = torch.max(outputs.data, 1)
-    #     print(predicted)
-    #     print(y)
-    #     print("Image {} predicted as {}".format(y, predicted[0]))
-    #     # tensor_image = torch.squeeze(x).to('cpu')
-    #     # print(tensor_image.shape)
-    #     # plt.imshow(tensor_image.permute(1, 2, 0))
-    #     # plt.show()
-    #     #
-    #     # import sys
-    #     # sys.exit()
+    print(pred_time_list)
 
 
 if __name__ == '__main__':
-    test()
+    main()
